beb0cc529c53c3558ecf5b65ce9563fedf2537aa
qztweII <terencexfwu@gmail.com>
2025-03-04 11:08:45 +1100

    Update main.py
    Added an element of randomness into the drive

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from random import randint
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -15,17 +16,17 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
-MotorL = Motor(Port.B)
-MotorR = Motor(Port.C)
+MotorL = Motor(Port.C)
+MotorR = Motor(Port.B)
 
 while True:
     # Write your program here.
     Driver = DriveBase(MotorL, MotorR, wheel_diameter = 55, axle_track = 104)
     for i in range(10):
-        Driver.straight(10)
+        Driver.straight(randint(10, 100))
         Driver.turn(36)
         ev3.speaker.beep()
-        Driver.straight(-20)
+        Driver.straight(randint(-10, -100))
 
     Driver.straight(100)
     Driver.straight(-100)
